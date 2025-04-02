1a27eed27a1358c02d8011e543b4949ce8e450a6
KaizenPrad <namandwivedi32@gmail.com>
2025-04-02 14:20:49 +0530

    Made Cards

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+        <>
+            <div className='CardBox h-50 w-20 bg-blue ' >
+
+            </div>
+        </>
+  )
+}
+
+export default Card
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Arise from '../components/Arise'
+import Card from '../components/Card'
 
 
 
@@ -10,8 +11,9 @@ const Home = () => {
    <div className='bg-black h-screen w-full'>
     <Navbar/>
     <Arise/>
-
-
+    <div className="CardContainer border-red-900 border-10 my-5 mx-5 px-4 py-4 ">
+    <Card/>
+    </div>
    </div>
    </>
   )
