4740d65f94ebeb18521678e88bbc36165952befe
KaizenPrad <namandwivedi32@gmail.com>
2025-04-02 14:54:09 +0530

    Made Changes in Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Card = () => {
   return (
         <>
-            <div className='CardBox h-50 w-20 bg-blue ' >
-
+            <div className='CardBox h-60 w-45 border-cyan-600 border-4 rounded-[8px]' >
+            <span className='name w-full  '>
+              <p className='bg-cyan-600 h-10 w-full flex justify-center items-center  CharName font-bold'>SUNG-JINWOO</p>
+            </span>
             </div>
         </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ import Card from '../components/Card'
 const Home = () => {
   return (
    <>
-   <div className='bg-black h-screen w-full'>
+   <div className='bg-black h-full w-full'>
     <Navbar/>
     <Arise/>
     <div className="CardContainer border-red-900 border-10 my-5 mx-5 px-4 py-4 ">
